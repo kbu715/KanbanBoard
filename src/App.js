572e6ca182b0a434ccd764329e875db460598940
paulkang <kbu715@gmail.com>
2021-02-02 12:41:02 +0900

    onDragEnd and persist reordering list

--- src/App.js
+++ src/App.js
@@ -4,10 +4,43 @@ import Column from "./components/Column";
 import { DragDropContext } from "react-beautiful-dnd";
 
 function App() {
-  const [data] = useState(initialData);
+  const [data, setData] = useState(initialData);
 
-  const onDragEnd = () => {
+  const onDragEnd = (result) => {
     //TODO: reorder out column
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = data.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+
+    newTaskIds.splice(source.index, 1); //from the first arg index, remove 1 item.
+    newTaskIds.splice(destination.index, 0, draggableId); // destination.index 위치에 draggableId 삽입
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds, // taskIds만 교체
+    };
+
+    const newData = {
+      ...data,
+      columns: {
+        ...data.columns,
+        [newColumn.id]: newColumn,
+      },
+    };
+
+    setData(newData);
   };
 
   return (
