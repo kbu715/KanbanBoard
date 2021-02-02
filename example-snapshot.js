cc044cd23da08cc30e1bee96c4d7b2efab491d55
paulkang <kbu715@gmail.com>
2021-02-02 12:56:38 +0900

    example

---
+++ example-snapshot.js
@@ -0,0 +1,11 @@
+//Draggable
+const draggableSnapshot = {
+  idDragging: true,
+  draggingOver: "column-1",
+};
+
+//Droppable
+const droppableSnapshot = {
+  idDraggingOver: true,
+  draggingOverWith: "task-1",
+};
